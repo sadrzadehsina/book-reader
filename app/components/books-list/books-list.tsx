5986f34c9f9dfa418fcc5cf6c4e6864a858fa069
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-08-03 16:28:56 +0330

    Change books list background

--- app/components/books-list/books-list.tsx
+++ app/components/books-list/books-list.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Text } from "@chakra-ui/react";
 
 export function BooksList({ books }: { books: Object[] }) {
   return books.map((book) => (
-    <Card key={book.title}>
+    <Card key={book.title} mt="4" backgroundColor="burlywood" color="black">
       <CardBody>
         <Text>{book.title}</Text>
       </CardBody>
