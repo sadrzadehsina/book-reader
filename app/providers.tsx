525074e96cf094eb0b9d1e662b2229e00ed4dd7f
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-08-29 06:24:10 +0330

    Add a few google fonts

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import '@fontsource/inter'
+import '@fontsource/roboto'
+
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { DatabaseProvider } from "./context/database";
 
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter', sans-serif`,
+    body: `'Roboto', sans-serif`,
+  },
+});
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <DatabaseProvider>
       <CacheProvider>
-        <ChakraProvider>{children}</ChakraProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </CacheProvider>
     </DatabaseProvider>
   );
