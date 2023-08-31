552012f5bbd37f4f9f3b28a85ba13b5899c9783b
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-08-31 06:11:31 +0330

    Add navigation to reader

---
+++ app/reader/components/navigation/index.tsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  ButtonGroup,
+  CloseButton,
+  HStack,
+  IconButton,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  LiaAngleLeftSolid,
+  LiaAngleRightSolid,
+  LiaBarsSolid,
+} from "react-icons/lia";
+import { useReader } from "../../use-reader";
+import { useSetScreen } from "@/app/hooks/use-screen";
+import { useCallback } from "react";
+
+export function Navigation() {
+  const { previous, next } = useReader();
+
+  const setScreen = useSetScreen();
+
+  const closeBook = useCallback(() => {
+    setScreen("dashboard");
+  }, []);
+
+  return (
+    <>
+      <ButtonGroup pos="absolute" top="5" right="5" zIndex="9">
+        <VStack>
+          <CloseButton onClick={closeBook} />
+          <IconButton
+            fontSize="24px"
+            aria-label="table-of-content"
+            variant="ghost"
+            icon={<LiaBarsSolid />}
+          />
+        </VStack>
+      </ButtonGroup>
+      <ButtonGroup
+        pos="absolute"
+        bottom="10"
+        right="50%"
+        marginRight="-40px"
+        zIndex="9"
+      >
+        <IconButton
+          fontSize="24px"
+          aria-label="previous"
+          icon={<LiaAngleLeftSolid />}
+          onClick={previous}
+        />
+        <IconButton
+          fontSize="24px"
+          aria-label="next"
+          icon={<LiaAngleRightSolid />}
+          onClick={next}
+        />
+      </ButtonGroup>
+    </>
+  );
+}
--- app/reader/index.tsx
+++ app/reader/index.tsx
@@ -3,6 +3,7 @@
 import { Box } from "@chakra-ui/react";
 import { useEffect, useRef } from "react";
 import { useReader } from "./use-reader";
+import { Navigation } from "./components/navigation";
 
 export function Reader() {
   const viewAreaRef = useRef(null);
@@ -15,6 +16,7 @@ export function Reader() {
 
   return (
     <Box pt="4" pb="4">
+      <Navigation />
       <div ref={viewAreaRef} style={{ height: '100vh'}} />
     </Box>
   );
