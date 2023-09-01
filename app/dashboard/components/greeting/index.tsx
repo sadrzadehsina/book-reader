0174a51ddc55493ac119fdb40b2c1c520130d22c
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-09-01 06:19:40 +0330

    Update greeting message to work with time

--- app/dashboard/components/greeting/index.tsx
+++ app/dashboard/components/greeting/index.tsx
@@ -1,5 +1,18 @@
 import { Text } from "@chakra-ui/react";
+import { useMemo } from "react";
 
 export function Greeting() {
-  return <Text variant="h1">Good Evening, Sina</Text>
+  const d = new Date();
+
+  const time = d.getHours();
+
+  const message = useMemo(() => {
+    if (time < 12) return "Good morning";
+
+    if (time > 12 && time < 18) return "Good afternoon";
+
+    return "Good night";
+  }, [time]);
+
+  return <Text variant="h1">{message}, Sina</Text>;
 }
